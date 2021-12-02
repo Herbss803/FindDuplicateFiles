dcb6040fa0a9e518fbfd36988489261c02155df8
Herbss803 <chow.herbert@gmail.com>
2021-12-01 20:21:12 -0800

    working with folds

---
+++ FileUtilities/FileUtilities.fs
@@ -0,0 +1,19 @@
+﻿namespace FileUtilities
+open System
+open System.IO
+
+module Finder =
+    let fileHasExtension ext filename = 
+        if String.Compare(Path.GetExtension(filename:string),ext, StringComparison.InvariantCultureIgnoreCase) = 0
+        then Some(filename)
+        else None
+    
+    let rec getFilesWithExtension ext filePath = 
+        let filterForExtension = fileHasExtension ext
+        let files = Directory.GetFiles filePath
+        let filesMatchingExtension = files |> Array.choose filterForExtension
+        let dirs = Directory.GetDirectories filePath
+        Array.fold (fun acc d -> 
+                    let subfolderResults = getFilesWithExtension ext d
+                    Array.append subfolderResults acc
+                    ) filesMatchingExtension dirs
